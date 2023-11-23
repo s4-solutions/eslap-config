b8ca939b74f7c9a974722cc7bb2b7c79c6f0267c
myjj1307 <myjj1307@gmail.com>
2023-11-23 10:46:32 +0900

    ..

--- install.sh
+++ install.sh
@@ -3,18 +3,27 @@
 systemctl stop eslap-config
 systemctl stop nginx
 
+rm -rf /usr/lib/4seils
+
 apt-get install -y nginx libpam0g-dev
 
-rm -rf /usr/lib/4seils
-mkdir /usr/lib/4seils
+rm -rf /tmp/4seils
+mkdir /tmp/4seils
 
 wget https://github.com/s4-solutions/eslap-config/raw/master/eslap-config.tgz
 
-tar xvfz eslap-config.tgz -C /usr/lib/4seils
+tar xvfz eslap-config.tgz -C /tmp/4seils
 
-cp -f /usr/lib/4seils/nginx.conf /etc/nginx
+chown eslap-1118:eslap-1118 -R /tmp/4seils
+chmod +x /tmp/4seils/eslap_pre.sh eslap-config
 
-cp -f /usr/lib/4seils/eslap-config.service /etc/systemd/system
+cp -rf /tmp/4seils/static_files /home/eslap-1118/eslap/bin
+cp -f /tmp/4seils/eslap-config /home/eslap-1118/eslap/bin
+cp -f /tmp/4seils/eslap_pre.sh /home/eslap-1118/eslap/bin
+
+cp -f /tmp/4seils/nginx.conf /etc/nginx
+
+cp -f /tmp/4seils/eslap-config.service /etc/systemd/system
 systemctl daemon-reload
 
 systemctl enable eslap-config
@@ -24,5 +33,6 @@ systemctl stop nginx
 systemctl start nginx
 
 rm -f eslap-config.tgz
+rm -rf /tmp/4seils
 
-
+systemctl disable unattended-upgrades
